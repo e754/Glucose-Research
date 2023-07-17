f7439a10d145338ded0fb1587304bb0df4ce7e0e
kteo123 <137189181+kteo123@users.noreply.github.com>
2023-07-17 16:38:53 -0400

    Update glucosequery.sql

--- src/1_sql/glucosequery.sql
+++ src/1_sql/glucosequery.sql
@@ -8,21 +8,18 @@ CREATE OR REPLACE TABLE `glucosedatabyicu.mergingFiltering.14_revisedtotalgluc` 
       a.total_glucose_measurements
     FROM (
       SELECT
-        hadm_id,
+        stay_id,
         COUNT(*) AS total_glucose_measurements
       FROM
         `glucose-390717.glucose1.actual_input_all`
       GROUP BY
-        hadm_id
+        stay_id
     ) a
     JOIN
       `glucosedatabyicu.mergingFiltering.comb` d
     ON
-      d.hadm_id = a.hadm_id
+      d.stay_id = a.stay_id
   ) a
   ORDER BY
     a.subject_id
 )
-
-
-  
