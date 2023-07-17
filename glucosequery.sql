8c6aa764953be69ca5e724fd6fff8865b40527d5
kteo123 <137189181+kteo123@users.noreply.github.com>
2023-07-17 09:09:58 -0400

    Add files via upload

---
+++ glucosequery.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE TABLE `glucosedatabyicu.mergingFiltering.14_revisedtotalgluc` AS (
+  SELECT
+    a.*,
+    IFNULL(CAST(total_glucose_measurements AS FLOAT64) / los, NULL) AS totalgluc_perLOS
+  FROM (
+    SELECT
+      d.*,
+      a.total_glucose_measurements
+    FROM (
+      SELECT
+        hadm_id,
+        COUNT(*) AS total_glucose_measurements
+      FROM
+        `glucose-390717.glucose1.actual_input_all`
+      GROUP BY
+        hadm_id
+    ) a
+    JOIN
+      `glucosedatabyicu.mergingFiltering.11_otherGluBool` d
+    ON
+      d.hadm_id = a.hadm_id
+  ) a
+  ORDER BY
+    a.subject_id
+)
+
+
+  
