c4052363c49cc5773ce9abbea41b229f09346053
e754 <59747919+e754@users.noreply.github.com>
2023-07-21 16:36:09 -0400

    Update cohortselection.py

--- src/2_cohorts/cohortselection.py
+++ src/2_cohorts/cohortselection.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 import numpy as np
-from utils import get_demography, print_demo, get_treatment_groups, comparte_resulting_cohort_datasets
 df = pd.read_csv("data/MIMIC.csv")
 
 
@@ -20,7 +19,6 @@ race=losTrim[losTrim['race_group']!='OTHER']
 race=race[race['race_group']!='Other']
 print(f"After removing those who stayed less than 1 day:{len(race)}")
 
-from google.colab import files
 
 # Save DataFrame to a CSV file
 race.to_csv('data/cohortedData.csv', index=False)
