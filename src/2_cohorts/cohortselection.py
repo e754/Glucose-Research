7a1f38a61d3d474f4df4845402b6136c6c219109
e754 <59747919+e754@users.noreply.github.com>
2023-08-01 11:50:30 -0400

    Update cohortselection.py

--- src/2_cohorts/cohortselection.py
+++ src/2_cohorts/cohortselection.py
@@ -10,6 +10,30 @@ def get_demo(a):
     print(f"Other race groups removed: {len(a[a['race_group']=='Other'])}")
 df = pd.read_csv("data/MIMIC.csv")
 
+df['isMale'] = 0
+df.loc[df['gender'] == 'M', 'isMale'] = 1
+
+def ckdStage(ckd):
+  return 1 if ckd >= 3 else 0
+df['ckd_stages'] = df['ckd_stages'].apply(ckdStage)
+
+def englishProf(language):
+  return 1 if language =='ENGLISH'  else 0
+df['english_Proficent'] = df['language'].apply(englishProf)
+
+def diabetes(diab):
+   if diab == 1 or diab == 2:
+      return 1
+   else:
+      return 0
+df['diabetes']=df['diabetes_types'].apply(diabetes)
+
+def otherRace(race):
+   if race=='OTHER':
+      return 'Other'
+   else:
+      return race
+df['race_group']=df['race_group'].apply(otherRace)
 
 ageTrim=df[df['age']>18]
 removed=df[df['age']<=18]
@@ -26,6 +50,7 @@ removed=sepTrim[sepTrim['los']<=1]
 print(f"After removing those who stayed less than 1 day:{len(losTrim)}")
 get_demo(removed)
 
+df=losTrim[losTrim['race_group']!='Other']
 df=losTrim[losTrim['race_group']!='Other']
 removed=losTrim[losTrim['race_group']=='Other']
 print(f"After removing those who stayed less than 1 day:{len(df)}")
@@ -66,8 +91,6 @@ df['temperature_mean'] = df['temperature_mean'].apply(lambda x: 0 if x > 45 else
 df['temperature_mean'] = df['temperature_mean'].apply(lambda x: 36.5 if x == 0 or pd.isna(x) else x)
 
 #fill in NA
-df.fillna(0, inplace=True)
 
 # Save DataFrame to a CSV file
 df.to_csv('data/cohortedData.csv', index=False)
-
