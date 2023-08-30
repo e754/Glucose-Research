e94a5d52c3635b299d04d8fa7e916a6d8040af2e
Yuri Jia <59747919+e754@users.noreply.github.com>
2023-08-30 10:44:33 -0400

    Update 3_onlyGlucose.sql

--- src/1_sql/3_onlyGlucose.sql
+++ src/1_sql/3_onlyGlucose.sql
@@ -1,2 +1,2 @@
-CREATE TABLE `glucosedatabyicu.measurmentTaken.onlyGlucose` AS
+CREATE TABLE `db_name.my_Mimic.onlyGlucose` AS
 select * from `physionet-data.mimiciv_icu.chartevents` where itemid IN (220621, 228388, 225664,226537);
