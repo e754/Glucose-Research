f329cab11d7bfc29ad97344189f9694dbfd809fa
Tristan <60614027+mirkompcr@users.noreply.github.com>
2024-02-11 15:03:30 +0100

    Update dagitty_glucose_freq.R
    updated one arrow from lab values

--- src/3_EDA/dagitty_glucose_freq.R
+++ src/3_EDA/dagitty_glucose_freq.R
@@ -1,7 +1,7 @@
 dag {
 "Admission type" [pos="-0.533,-0.885"]
 "Anchor Year" [pos="-0.600,-0.482"]
-"English Proficiency" [pos="-2.011,0.409"]
+"English Proficiency" [pos="-1.912,0.383"]
 "Glucose frequency" [outcome,pos="0.719,0.155"]
 "Glucose measured" [pos="-0.248,0.464"]
 "Insurance type" [pos="-1.681,-0.457"]
@@ -13,7 +13,7 @@ CCI [pos="-0.761,0.530"]
 COPD [pos="1.227,0.617"]
 Comorbidities [pos="-0.141,0.854"]
 Demographics [exposure,pos="-1.337,0.162"]
-Diabetes [pos="1.327,0.923"]
+Diabetes [pos="1.322,0.837"]
 Fluids [pos="-0.100,-1.525"]
 Glucocorticoids [pos="1.141,-1.253"]
 Insulin [pos="-0.409,0.315"]
@@ -28,7 +28,6 @@ Vitals [pos="0.582,-1.337"]
 "Glucose frequency" <-> "Glucose measured"
 "Glucose frequency" <-> Insulin
 "Insurance type" -> Demographics
-"Lab values" -> "Glucose frequency"
 "Lab values" -> SOFA
 "SOFA components" -> SOFA
 Age -> Demographics
