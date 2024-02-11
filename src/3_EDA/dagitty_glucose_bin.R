0bafd60d58a1c89a973243ef5b2767e6df3b2a37
Tristan <60614027+mirkompcr@users.noreply.github.com>
2024-02-11 15:04:10 +0100

    Update dagitty_glucose_bin.R
    removed arrow from lab values

--- src/3_EDA/dagitty_glucose_bin.R
+++ src/3_EDA/dagitty_glucose_bin.R
@@ -1,7 +1,7 @@
 dag {
 "Admission type" [pos="-0.533,-0.885"]
 "Anchor Year" [pos="-0.600,-0.482"]
-"English Proficiency" [pos="-2.011,0.409"]
+"English Proficiency" [pos="-1.922,0.372"]
 "Glucose measured y/n" [outcome,pos="0.719,0.155"]
 "Insurance type" [pos="-1.681,-0.457"]
 "Lab values" [pos="0.684,-1.000"]
@@ -12,7 +12,7 @@ CCI [pos="-0.761,0.530"]
 COPD [pos="1.227,0.617"]
 Comorbidities [pos="-0.141,0.854"]
 Demographics [exposure,pos="-1.337,0.162"]
-Diabetes [pos="1.327,0.923"]
+Diabetes [pos="1.331,0.801"]
 Fluids [pos="-0.100,-1.525"]
 Glucocorticoids [pos="1.141,-1.253"]
 Insulin [pos="1.375,0.085"]
@@ -26,7 +26,6 @@ Vitals [pos="0.582,-1.337"]
 "English Proficiency" -> Demographics
 "Glucose measured y/n" -> Insulin
 "Insurance type" -> Demographics
-"Lab values" -> "Glucose measured y/n"
 "Lab values" -> SOFA
 "SOFA components" -> SOFA
 Age -> Demographics
